41f2e0387354b4ddc5e6206f4e79d051c42ed707
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2026-02-18 20:23:14 -0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,81 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { buildRoutePath } from "./utils/build-route-path.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { search } = req.query;
+
+      const tasks = database.select(
+        "tasks",
+        search
+          ? {
+              title: search,
+              description: search,
+            }
+          : null,
+      );
+
+      return res.end(JSON.stringify(tasks));
+    },
+  },
+  {
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const actualDate = new Date();
+
+      if (!title) {
+        return res
+          .writeHead(400)
+          .end(JSON.stringify({ error: "Title is required" }));
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: actualDate,
+        updated_at: actualDate,
+      };
+
+      database.insert("tasks", task);
+
+      return res.writeHead(201).end();
+    },
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+
+      database.update("tasks", id, {
+        title,
+        description,
+      });
+
+      return res.writeHead(204).end();
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete("tasks", id);
+
+      return res.writeHead(204).end();
+    },
+  },
+];
