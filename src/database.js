a33be0bf22a8121e49727554d0117fe993c04254
Jandir Luiz Habitzreuter <dyjandir@hotmail.com>
2026-02-23 18:30:11 -0300

    feat: Update validation

--- src/database.js
+++ src/database.js
@@ -1,65 +1,81 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const databasePath = new URL('../db.json', import.meta.url)
+const databasePath = new URL("../db.json", import.meta.url);
 
 export class Database {
-  #database = {}
+  #database = {};
 
   constructor() {
-    fs.readFile(databasePath, 'utf8')
-      .then(data => {
-        this.#database = JSON.parse(data)
+    fs.readFile(databasePath, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
       })
       .catch(() => {
-        this.#persist()
-      })
+        this.#persist();
+      });
   }
 
   #persist() {
-    fs.writeFile(databasePath, JSON.stringify(this.#database))
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
   select(table, search) {
-    let data = this.#database[table] ?? []
+    let data = this.#database[table] ?? [];
 
     if (search) {
-      data = data.filter(row => {
+      data = data.filter((row) => {
         return Object.entries(search).some(([key, value]) => {
-          return row[key].toLowerCase().includes(value.toLowerCase())
-        })
-      })
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
     }
 
-    return data
+    return data;
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data)
+      this.#database[table].push(data);
     } else {
-      this.#database[table] = [data]
+      this.#database[table] = [data];
     }
 
-    this.#persist()
+    this.#persist();
 
-    return data
+    return data;
   }
 
   update(table, id, data) {
-    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
-      this.#database[table][rowIndex] = { id, ...data }
-      this.#persist()
+      const currentRow = this.#database[table][rowIndex];
+
+      const hasTitleChange =
+        data.title !== undefined && data.title !== currentRow.title;
+
+      const hasDescriptionChange =
+        data.description !== undefined &&
+        data.description !== currentRow.description;
+
+      if (hasTitleChange || hasDescriptionChange)
+        this.#database[table][rowIndex] = {
+          ...currentRow,
+          title: data.title ?? currentRow.title,
+          description: data.description ?? currentRow.description,
+          updated_at: new Date(),
+        };
+
+      this.#persist();
     }
   }
 
   delete(table, id) {
-    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
 
     if (rowIndex > -1) {
-      this.#database[table].splice(rowIndex, 1)
-      this.#persist()
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
     }
   }
 }
